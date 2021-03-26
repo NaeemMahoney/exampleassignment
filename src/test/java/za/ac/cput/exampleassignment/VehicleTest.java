49740b3ba6b4e9a875d9bceb9528a9cb0649f683
Tye <tyewalker09@gmail.com>
2021-03-26 17:21:00 +0200

    Added VehicleTest and Vehicle

---
+++ src/test/java/za/ac/cput/exampleassignment/VehicleTest.java
@@ -0,0 +1,12 @@
+package za.ac.cput.exampleassignment;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
